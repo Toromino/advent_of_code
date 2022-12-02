ec5a2ed255d784873daa96d6058184ae1ff41113
Toromino <foxhkron@cybre.club>
2022-12-02 18:18:45 +0100

    Improve formatting and readability of day 2

--- 2022/02/part_one.c
+++ 2022/02/part_one.c
@@ -9,58 +9,79 @@
  *   A, X: Rock
  *   B, Y: Paper
  *   C, Z: Scissors
- * 
+ *
  * c >= 'A' && c <= 'C' are the letters of your opponent
  * c >= 'X' && c <= 'Z' are your letters
- * */
+ */
 
-int calculate_score(char a, char b) {
-    int score = 0;
+#define OPPONENT_ROCK     'A'
+#define OPPONENT_PAPER    'B'
+#define OPPONENT_SCISSORS 'C'
+
+#define PLAYER_ROCK       'X'
+#define PLAYER_PAPER      'Y'
+#define PLAYER_SCISSORS   'Z'
+
+/* Calculate your player's total score*/
+unsigned int calculate_score(char a, char b) {
+    unsigned int score = 0;
 
     /* base score: 1 for Rock, 2 for paper, 3 for scissors */
-    score = b == 'X' ? 1 : b == 'Y' ? 2 : b == 'Z' ? 3 : 0;
+    score = b == PLAYER_ROCK     ? 1
+          : b == PLAYER_PAPER    ? 2
+          : b == PLAYER_SCISSORS ? 3
+          : 0;
 
     /* add 6 pts to the score if: Rock - Paper, Paper - Scis., Sciss. - Rock */
-    if ((a == 'A' && b == 'Y') || (a == 'B' && b == 'Z') || (a == 'C' && b == 'X'))
+    if ((a == OPPONENT_ROCK     && b == PLAYER_PAPER) ||
+        (a == OPPONENT_PAPER    && b == PLAYER_SCISSORS) ||
+        (a == OPPONENT_SCISSORS && b == PLAYER_ROCK)) {
         score += 6;
+    }
     /* add 3 pts to the score if: Rock - Rock, Paper - Paper, Sciss. - Sciss.*/
-    else if((a == 'A' && b == 'X') || (a == 'B' && b == 'Y') || (a == 'C' && b == 'Z'))
+    else if ((a == OPPONENT_ROCK     && b == PLAYER_ROCK) ||
+             (a == OPPONENT_PAPER    && b == PLAYER_PAPER) ||
+             (a == OPPONENT_SCISSORS && b == PLAYER_SCISSORS)) {
         score += 3;
+    }
 
     return score;
 }
 
 int part_one() {
-    FILE* file_ptr;
-
-    file_ptr = fopen("input.txt", "r");
-    if(!file_ptr)
-        exit(EXIT_FAILURE);
-
-    int player_score = 0;
-
+    FILE *file_ptr;
+    unsigned int player_score = 0;
     char col_a, col_b; /* letters in the columns of the current line */
     char c, last_c;    /* current character and last character */
 
-    while((c = fgetc(file_ptr)) != EOF) {
-        if(c == '\n') {
+    file_ptr = fopen("input.txt", "r");
+    if (!file_ptr) {
+        exit(EXIT_FAILURE);
+        fprintf(stderr, "File could not be read!");
+    }
+
+    while ((c = fgetc(file_ptr)) != EOF) {
+        if (c == '\n') {
             player_score += calculate_score(col_a, col_b);
             col_a = col_b = 0;
-        } else if((c >= 'A' && c <= 'C') || (c >= 'X' && c <= 'Z')) {
-            if(last_c == ' ')
+        } else if ((c >= 'A' && c <= 'C') || (c >= 'X' && c <= 'Z')) {
+            if (last_c == ' ') {
                 col_b = c;
-            else
+            } else {
                 col_a = c;
+            }
         }
 
-        if (c != '\r') /* ignore CR (Windows files) */
+        if (c != '\r') {/* ignore CR (Windows files) */
             last_c = c;
+        }
     }
     fclose(file_ptr);
 
-    printf("What would your total score be if everything goes exactly according to your strategy guide? %d\n", player_score);
+    printf("What would your total score be if everything goes exactly \
+according to your strategy guide? %d\n", player_score);
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     part_one();
 }
--- 2022/02/part_two.c
+++ 2022/02/part_two.c
@@ -9,24 +9,41 @@
  *   A, X: Rock
  *   B, Y: Paper
  *   C, Z: Scissors
- * 
+ *
  * c >= 'A' && c <= 'C' are the letters of your opponent
  * c >= 'X' && c <= 'Z' are your letters
- * */
+ */
+
+#define OPPONENT_ROCK     'A'
+#define OPPONENT_PAPER    'B'
+#define OPPONENT_SCISSORS 'C'
+
+#define PLAYER_ROCK       'X'
+#define PLAYER_PAPER      'Y'
+#define PLAYER_SCISSORS   'Z'
 
 /* Calculate your player's total score*/
-int calculate_score(char a, char b) {
-    int score = 0;
+unsigned int calculate_score(char a, char b) {
+    unsigned int score = 0;
 
     /* base score: 1 for Rock, 2 for paper, 3 for scissors */
-    score = b == 'X' ? 1 : b == 'Y' ? 2 : b == 'Z' ? 3 : 0;
+    score = b == PLAYER_ROCK     ? 1
+          : b == PLAYER_PAPER    ? 2
+          : b == PLAYER_SCISSORS ? 3
+          : 0;
 
     /* add 6 pts to the score if: Rock - Paper, Paper - Scis., Sciss. - Rock */
-    if ((a == 'A' && b == 'Y') || (a == 'B' && b == 'Z') || (a == 'C' && b == 'X'))
+    if ((a == OPPONENT_ROCK     && b == PLAYER_PAPER) ||
+        (a == OPPONENT_PAPER    && b == PLAYER_SCISSORS) ||
+        (a == OPPONENT_SCISSORS && b == PLAYER_ROCK)) {
         score += 6;
+    }
     /* add 3 pts to the score if: Rock - Rock, Paper - Paper, Sciss. - Sciss.*/
-    else if((a == 'A' && b == 'X') || (a == 'B' && b == 'Y') || (a == 'C' && b == 'Z'))
+    else if ((a == OPPONENT_ROCK     && b == PLAYER_ROCK) ||
+             (a == OPPONENT_PAPER    && b == PLAYER_PAPER) ||
+             (a == OPPONENT_SCISSORS && b == PLAYER_SCISSORS)) {
         score += 3;
+    }
 
     return score;
 }
@@ -36,48 +53,63 @@ int calculate_score(char a, char b) {
  *
  * a: first column, b: second column
  */
-char strategic_response (char a, char b) {
-    if (b == 'X')
+char strategic_response(char a, char b) {
+    if (b == 'X') {
         /* Needs to lose: Rock -> Scissors, Paper -> Rock, Scissors -> Paper*/
-        return a == 'A' ? 'Z' : a == 'B' ? 'X' : a == 'C' ? 'Y' : 0;
-    else if (b == 'Y')
+        return a == OPPONENT_ROCK     ? PLAYER_SCISSORS
+             : a == OPPONENT_PAPER    ? PLAYER_ROCK
+             : a == OPPONENT_SCISSORS ? PLAYER_PAPER
+             : 0;
+    } else if (b == 'Y') {
         /* needs a draw: Rock -> Rock, Paper -> Paper, Scissors -> Scissors*/
-        return a == 'A' ? 'X' : a == 'B' ? 'Y' : a == 'C' ? 'Z' : 0;
-    else if (b == 'Z')
+        return a == OPPONENT_ROCK     ? PLAYER_ROCK
+             : a == OPPONENT_PAPER    ? PLAYER_PAPER
+             : a == OPPONENT_SCISSORS ? PLAYER_SCISSORS
+             : 0;
+    } else if (b == 'Z') {
         /* needs to win: Rock -> Paper, Paper -> Scissors, Scissors -> Rock*/
-        return a == 'A' ? 'Y' : a == 'B' ? 'Z' : a == 'C' ? 'X' : 0;
+        return a == OPPONENT_ROCK     ? PLAYER_PAPER
+             : a == OPPONENT_PAPER    ? PLAYER_SCISSORS
+             : a == OPPONENT_SCISSORS ? PLAYER_ROCK
+             : 0;
+    }
 }
 
 int part_two() {
-    FILE* file_ptr;
-
-    file_ptr = fopen("input.txt", "r");
-    if(!file_ptr)
-        exit(EXIT_FAILURE);
-
-    int player_score = 0;
+    FILE *file_ptr;
+    unsigned int player_score = 0;
     char col_a, col_b; /* letters in the columns of the current line */
     char c, last_c;    /* current character and last character */
 
-    while((c = fgetc(file_ptr)) != EOF) {
-        if(c == '\n') {
+    file_ptr = fopen("input.txt", "r");
+    if (!file_ptr) {
+        exit(EXIT_FAILURE);
+        fprintf(stderr, "File could not be read!");
+    }
+
+    while ((c = fgetc(file_ptr)) != EOF) {
+        if (c == '\n') {
             player_score += calculate_score(col_a, strategic_response(col_a, col_b));
             col_a = col_b = 0;
-        } else if((c >= 'A' && c <= 'C') || (c >= 'X' && c <= 'Z')) {
-            if(last_c == ' ')
+        } else if ((c >= 'A' && c <= 'C') || (c >= 'X' && c <= 'Z')) {
+            if (last_c == ' ') {
                 col_b = c;
-            else
+            } else {
                 col_a = c;
+            }
         }
 
-        if (c != '\r') /* ignore CR (Windows files) */
+        if (c != '\r') {/* ignore CR (Windows files) */
             last_c = c;
+        }
     }
     fclose(file_ptr);
 
-    printf("Following the Elf's instructions for the second column, what would your total score be if everything goes exactly according to your strategy guide? %d\n", player_score);
+    printf("Following the Elf's instructions for the second column, what \
+would your total score be if everything goes exactly according to your \
+strategy guide? %d\n", player_score);
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     part_two();
 }
